db1ec23e5a01e5c388de2d92de7a41ef565e35b3
pia <mariapianavach@gmail.com>
2021-09-28 14:21:02 +0200

    My calculatrice

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-function calculatrice(num) {
-    var id_resultat = document.getElementById("resultat");
-    id_resultat.innerHTML += num
+let resultat = document.getElementById('resultat')
+
+function appuisTouche(num) {
+    resultat.innerHTML += num
+}
+function appuisSymbole(sym) {
+    let pExiste = resultat.innerHTML.indexOf('+')
+    let mExiste = resultat.innerHTML.indexOf('-')
+    let fExiste = resultat.innerHTML.indexOf('X')
+    let dExiste = resultat.innerHTML.indexOf('/')
+
+    if (pExiste == -1 && mExiste == -1 && fExiste == -1 && dExiste == -1) {
+        resultat.innerHTML += sym
+    }
 }
 
+function appuisClear() {
+    resultat.innerHTML = ""
+}

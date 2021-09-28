04ec098d93637fcf9727b762776fd9852cb215d2
pia <mariapianavach@gmail.com>
2021-09-28 15:30:40 +0200

    My calculatrice

--- index.js
+++ index.js
@@ -17,3 +17,30 @@ function appuisSymbole(sym) {
 function appuisClear() {
     resultat.innerHTML = ""
 }
+
+function egal() {
+    let pExiste = resultat.innerHTML.indexOf('+')
+    let mExiste = resultat.innerHTML.indexOf('-')
+    let fExiste = resultat.innerHTML.indexOf('*')
+    let dExiste = resultat.innerHTML.indexOf('/')
+
+    if(pExiste>-1){
+        let nombre =resultat.innerHTML.split('+')
+        resultat.innerHTML=(+nombre[0])+(+nombre[1])
+    }
+
+    if (mExiste > -1) {
+        let nombre = resultat.innerHTML.split('-')
+        resultat.innerHTML = (nombre[0]) - (nombre[1])
+    }
+   
+    if (fExiste > -1) {
+        let nombre = resultat.innerHTML.split('*')
+        resultat.innerHTML = (nombre[0]) * (nombre[1])
+    }
+
+    if (dExiste > -1) {
+        let nombre = resultat.innerHTML.split('/')
+        resultat.innerHTML = (nombre[0]) / (nombre[1])
+    }
+}
